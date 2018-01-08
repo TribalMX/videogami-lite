d35c6d91d00112ca0cff4d35c0582ef48c822b24
Ricky <rickysychan@gmail.com>
2018-01-07 22:57:53 -0500

    added scheduling fecheduling feature and ability to change input and rtmp keys

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -1,43 +1,177 @@
-let express = require('express')
-let router = express.Router()
-let cmd = require('node-cmd')
+const express = require('express')
+const router = express.Router()
+const cmd = require('node-cmd')
+const schedule = require('node-schedule')
+
+let YTrtmpKey = 'rw74-map3-gtjy-d9sh'
+let JCrtmpKey = 'rickysychan-7hup2-mqxsa-b6kmd-gj2gq'
+let JCInputURL = 'https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8'
+let FBInputURL = 'https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8'
+let YTInputURL = 'https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8'
+let FBrtmp = null
 
 // this is for joicaster
-// let convert = () => { cmd.run('ffmpeg -re -i https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8 -i ACE.png -i logo2.jpg -filter_complex "[1]scale=40:40[ovrl1], [0:v][ovrl1] overlay=580:10:enable=\'between(t,1,5)\'[v1];[2]scale=40:40[ovrl2], [v1][ovrl2] overlay=580:10:enable=\'between(t,5,15)\'[v2];[v2] drawtext=fontfile=fontfile=/System/Library/Fonts/Keyboard.ttf: text=\'VideoGami\':fontcolor=white: fontsize=24: x=(w-text_w)/2: y=(h-text_h)/1.05: enable=\'between(t,1,10)\'" -acodec aac -vcodec libx264 -f flv ' + '"rtmp://ingest-us-east.a.switchboard.zone/live/rickysychan-7hup2-mqxsa-b6kmd-gj2gq"') }
+let streamJC = () => { cmd.run('ffmpeg -re -i ' + JCInputURL + ' -i ACE.png -i logo2.jpg -filter_complex "[1]scale=40:40[ovrl1], [0:v][ovrl1] overlay=580:10:enable=\'between(t,1,5)\'[v1];[2]scale=40:40[ovrl2], [v1][ovrl2] overlay=580:10:enable=\'between(t,5,15)\'[v2];[v2] drawtext=fontfile=fontfile=/System/Library/Fonts/Keyboard.ttf: text=\'VideoGami\':fontcolor=white: fontsize=24: x=(w-text_w)/2: y=(h-text_h)/1.05: enable=\'between(t,1,10)\'" -acodec aac -vcodec libx264 -f flv ' + '"rtmp://ingest-us-east.a.switchboard.zone/live/' + JCrtmpKey + '"') }
 
 // this is for faebook only
-// let convert = () => { cmd.run('ffmpeg -re -i "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8" -i ACE.png -i logo2.jpg -filter_complex "[1]scale=40:40[ovrl1], [0:v][ovrl1] overlay=580:10:enable=\'between(t,1,5)\'[v1];[2]scale=40:40[ovrl2], [v1][ovrl2] overlay=580:10:enable=\'between(t,5,15)\'[v2];[v2] drawtext=fontfile=fontfile=/System/Library/Fonts/Keyboard.ttf: text=\'VideoGami\':fontcolor=white: fontsize=24: x=(w-text_w)/2: y=(h-text_h)/1.05: enable=\'between(t,1,10)\'" -acodec aac -vcodec libx264 -f flv ' + '"' + rtmp + '"') }
+let streamFB = () => { cmd.run('ffmpeg -re -i ' + FBInputURL + ' -i ACE.png -i logo2.jpg -filter_complex "[1]scale=40:40[ovrl1], [0:v][ovrl1] overlay=580:10:enable=\'between(t,1,5)\'[v1];[2]scale=40:40[ovrl2], [v1][ovrl2] overlay=580:10:enable=\'between(t,5,15)\'[v2];[v2] drawtext=fontfile=fontfile=/System/Library/Fonts/Keyboard.ttf: text=\'VideoGami\':fontcolor=white: fontsize=24: x=(w-text_w)/2: y=(h-text_h)/1.05: enable=\'between(t,1,10)\'" -acodec aac -vcodec libx264 -f flv ' + '"' + FBrtmp + '"') }
 
 // this is for Youtube only
-let convert = () => { cmd.run('ffmpeg -re -i "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8" -i ACE.png -i logo2.jpg -filter_complex "[1]scale=40:40[ovrl1], [0:v][ovrl1] overlay=580:10:enable=\'between(t,1,5)\'[v1];[2]scale=40:40[ovrl2], [v1][ovrl2] overlay=580:10:enable=\'between(t,5,15)\'[v2];[v2] drawtext=fontfile=fontfile=/System/Library/Fonts/Keyboard.ttf: text=\'VideoGami\':fontcolor=white: fontsize=24: x=(w-text_w)/2: y=(h-text_h)/1.05: enable=\'between(t,1,10)\'" -acodec aac -vcodec libx264 -f flv ' + '"rtmp://a.rtmp.youtube.com/live2/rw74-map3-gtjy-d9sh"') }
+let streamYT = () => { cmd.run('ffmpeg -re -i ' + YTInputURL + ' -i ACE.png -i logo2.jpg -filter_complex "[1]scale=40:40[ovrl1], [0:v][ovrl1] overlay=580:10:enable=\'between(t,1,5)\'[v1];[2]scale=40:40[ovrl2], [v1][ovrl2] overlay=580:10:enable=\'between(t,5,15)\'[v2];[v2] drawtext=fontfile=fontfile=/System/Library/Fonts/Keyboard.ttf: text=\'VideoGami\':fontcolor=white: fontsize=24: x=(w-text_w)/2: y=(h-text_h)/1.05: enable=\'between(t,1,10)\'" -acodec aac -vcodec libx264 -f flv ' + '"rtmp://a.rtmp.youtube.com/live2/' + YTrtmpKey + '"') }
 
 // this is to stop all ffmpeg activity
 
 let stop = () => { cmd.run('killall ffmpeg') }
-
-let rtmp = null
-// let rtmpYoutube = rtmp://a.rtmp.youtube.com/live2/rw74-map3-gtjy-d9sh
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'title' })
 })
 
-router.get('/convert', function (req, res, next) {
-  res.render('index', convert())
+// JC User rtmp key 
+
+router.post('/JCRtmpKey', function (req, res, next) {
+  JCrtmpKey = req.body.JCRtmpKey
+  res.redirect('/')
 })
 
-router.get('/stop', function (req, res, next) {
-  res.render('index', stop())
+// JC input 
+
+router.post('/JCInput', function (req, res, next) {
+  JCInputURL = req.body.JCInput
+  res.redirect('/')
 })
-router.get('/stop', function (req, res, next) {
-  res.render('index', stop())
+
+// stream JC Now
+
+router.get('/streamJC', function (req, res, next) {
+  streamJC()
+  res.redirect('/')
 })
+
+// stream JC scheduled
+
+let scheduleJC = null
+
+router.post('/streamJCScheduled', function (req, res, next) {
+  let month = req.body.month
+  let day = req.body.day
+  let hour = req.body.hour
+  let minute = req.body.minute
+  let date = new Date(2018, 0, 7, 22, minute)
+  console.log('schedule on ' + req.body.minute)
+  scheduleJC = schedule.scheduleJob(date, function () {
+    console.log('stream started')
+    streamJC()
+    scheduleJC.cancel()
+  })
+  res.redirect('/')
+})
+
+// cancel JC scheduled task
+
+router.post('/CancelstreamJCScheduled', function (req, res, next) {
+  console.log('canceled')
+  scheduleJC.cancel()
+  res.redirect('/')
+})
+
+// stop all ffmpeg tasks
+
+router.get('/stop', function (req, res, next) {
+  stop()
+  res.redirect('/')
+})
+
+// FB input
+
+router.post('/FBInput', function (req, res, next) {
+  FBInputURL = req.body.FBInput
+  res.redirect('/')
+})
+
+// stream to FB now
 
 router.post('/fbstream', function (req, res, next) {
-  rtmp = req.body.data
-  convert()
-  res.render('index')
+  FBrtmp = req.body.rtmplink
+  streamFB()
+  res.redirect('/')
+})
+
+// schedule FB stream
+
+let scheduleFB = null
+
+router.post('/streamFBScheduled', function (req, res, next) {
+  let month = req.body.month
+  let day = req.body.day
+  let hour = req.body.hour
+  let minute = req.body.minute
+  let date = new Date(2018, 0, 7, 22, minute)
+  console.log('schedule on ' + req.body.minute)
+  scheduleFB = schedule.scheduleJob(date, function () {
+    console.log('stream started.')
+    FBrtmp = req.body.rtmplink
+    streamFB()
+    scheduleFB.cancel()
+  })
+  res.redirect('/')
+})
+
+// cancel FB stream
+
+router.post('/CancelstreamFBScheduled', function (req, res, next) {
+  console.log('canceled')
+  scheduleFB.cancel()
+  res.redirect('/')
+})
+
+// JC User rtmp key 
+
+router.post('/YTRtmpKey', function (req, res, next) {
+  YTrtmpKey = req.body.YTRtmpKey
+  res.redirect('/')
+})
+
+// YT input
+
+router.post('/YTInput', function (req, res, next) {
+  YTInputURL = req.body.YTInput
+  res.redirect('/')
+})
+
+// Stream to YT now
+
+router.get('/streamYT', function (req, res, next) {
+  streamYT()
+  res.redirect('/')
+})
+
+// Schedule stream to YT
+
+let scheduleYT = null
+
+router.post('/streamYTScheduled', function (req, res, next) {
+  let month = req.body.month
+  let day = req.body.day
+  let hour = req.body.hour
+  let minute = req.body.minute
+  let date = new Date(2018, 0, 7, 22, minute)
+  console.log('schedule on ' + req.body.minute)
+  scheduleYT = schedule.scheduleJob(date, function () {
+    console.log('stream started.')
+    streamYT()
+    scheduleYT.cancel()
+  })
+  res.redirect('/')
+})
+
+// cancel schedled stream to YT
+
+router.post('/CancelstreamYTScheduled', function (req, res, next) {
+  console.log('canceled')
+  scheduleYT.cancel()
+  res.redirect('/')
 })
 
 module.exports = router
